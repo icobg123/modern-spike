00118881df82d0f66fe232e9caba39f832299406
icobg123 <gaby.antova@gmail.com>
2019-07-06 15:27:48 +0100

    Change only to stage

--- app.py
+++ app.py
@@ -13,8 +13,13 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def hello():
+    return "Hello World!"
+
+
+@app.route('/<name>')
+def hello_name(name):
+    return "Hello {}!".format(name)
 
 
 if __name__ == '__main__':
