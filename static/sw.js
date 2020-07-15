45fcfe1d0f3f590d58eb2c2eb61ad34cfb57a2da
Hristo <hristo.meshinski@gmail.com>
2020-07-15 16:17:40 +0100

    updte sw

--- static/sw.js
+++ static/sw.js
@@ -1,54 +1,57 @@
-var CACHE_NAME = 'static-cache';
-var urlsToCache = [
+// var CACHE_NAME = 'static-cache';
+// var urlsToCache = [
+//     '/static/bootstrap/js/jquery-3.4.1.min.js',
+//     '/static/bootstrap/js/bootstrap.min.js',
+//     '/static/js/ajax.js',
+//     '/static/js/main.js',
+//     '/static/bootstrap/css/bootstrap.css',
+// ];
+// version track
+const vn = "version-x";
+
+// files to cache
+const appCash = [
     '/static/bootstrap/js/jquery-3.4.1.min.js',
     '/static/bootstrap/js/bootstrap.min.js',
     '/static/js/ajax.js',
     '/static/js/main.js',
     '/static/bootstrap/css/bootstrap.css',
+    '/static/css/mana.css',
+    '/static/css/styles.css',
+    '/static/images/mana.svg',
 ];
 
-self.addEventListener('install', function (event) {
-    // Perform install steps
-    event.waitUntil(
-        caches.open(CACHE_NAME)
-            .then(function (cache) {
-                console.log('Opened cache');
-                return cache.addAll(urlsToCache);
-            })
+// install and save files to cache
+self.addEventListener('install', (e) => {
+    e.waitUntil(
+        caches.open(vn).then((cache) => {
+            return cache.addAll(appCash);
+        })
     );
 });
 
-self.addEventListener('fetch', function (event) {
-    event.respondWith(
-        caches.match(event.request)
-            .then(function (response) {
-                // Cache hit - return response
-                if (response) {
-                    return response;
-                }
-
-                return fetch(event.request).then(
-                    function (response) {
-                        // Check if we received a valid response
-                        if (!response || response.status !== 200 || response.type !== 'basic') {
-                            return response;
-                        }
-
-                        // IMPORTANT: Clone the response. A response is a stream
-                        // and because we want the browser to consume the response
-                        // as well as the cache consuming the response, we need
-                        // to clone it so we have two streams.
-                        var responseToCache = response.clone();
-
-                        caches.open(CACHE_NAME)
-                            .then(function (cache) {
-                                cache.put(event.request, responseToCache);
-                            });
-
-                        return response;
+// Listen for messages from client
+self.addEventListener('message', (event) => {
+    if (event.data.action === 'skipWaiting') {
+        self.skipWaiting();
+    } else if (event.data.action === 'clearOld') {
+        // Delete the old caches
+        event.waitUntil(
+            caches.keys().then((keys) => Promise.all(
+                keys.map((k) => {
+                    if (!k.includes(vn)) {
+                        return caches.delete(k);
                     }
-                );
+                })
+            )).then(() => {
+                console.log('old caches are cleared');
             })
-    );
+        )
+    }
 });
 
+// Serve if request already exists in cache else fetch
+self.addEventListener('fetch', e => {
+    const url = new URL(e.request.url);
+    (appCash.includes(url.pathname)) ? e.respondWith(caches.match(url)) : console.log("Fetching: " + url);
+});
