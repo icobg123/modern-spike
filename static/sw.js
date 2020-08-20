f4bb783932b5dae27bc8717fab09d76a908e7dbc
Hristo <hristo.meshinski@gmail.com>
2020-08-20 16:10:38 +0100

    update btn alignment

--- static/sw.js
+++ static/sw.js
@@ -1,6 +1,6 @@
 // This is based on the First Progressive Web App Tutorial by Google
 // https://codelabs.developers.google.com/codelabs/your-first-pwapp/
-const cacheName = 'modern-spike-PWA-23.l.111';
+const cacheName = 'modern-spike-PWA-23.l.1411';
 const filesToCache = [
     '/static/bootstrap/js/jquery-3.5.1.min.js',
     '/static/bootstrap/js/bootstrap.min.js',
