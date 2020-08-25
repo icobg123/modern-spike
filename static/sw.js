e51e04eb22986e141fa400a9d8d4edb493b7ea52
Hristo <hristo.meshinski@gmail.com>
2020-08-25 15:23:08 +0100

    don't show tournament url on really small devices

--- static/sw.js
+++ static/sw.js
@@ -1,6 +1,6 @@
 // This is based on the First Progressive Web App Tutorial by Google
 // https://codelabs.developers.google.com/codelabs/your-first-pwapp/
-const cacheName = 'modern-spike-PWA-.32231561s';
+const cacheName = 'modern-spike-PWA-11.32231561s';
 const filesToCache = [
     '/static/bootstrap/js/jquery-3.5.1.min.js',
     '/static/bootstrap/js/bootstrap.min.js',
