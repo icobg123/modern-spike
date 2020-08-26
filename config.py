2e7de062bc4dfb4d094acdb3212130700f5dce58
Hristo <hristo.meshinski@gmail.com>
2020-08-26 19:24:21 +0100

    change to mongo atlas db again...

--- config.py
+++ config.py
@@ -8,15 +8,19 @@ class Config(object):
     TESTING = False
     CSRF_ENABLED = True
     SECRET_KEY = 'this-really-needs-to-be-changed'
-    MONGO_HOST = os.environ['MONGO_HOST']
-    MONGO_PORT = os.environ['MONGO_PORT']
-    MONGO_DB = os.environ['MONGO_DB']
-    MONGO_USER = os.environ['MONGO_USER']
-    MONGO_PASS = os.environ['MONGO_PASS']
+    # MONGO_HOST = os.environ['MONGO_HOST']
+    # MONGO_PORT = os.environ['MONGO_PORT']
+    # MONGO_DB = os.environ['MONGO_DB']
+    # MONGO_USER = os.environ['MONGO_USER']
+    # MONGO_PASS = os.environ['MONGO_PASS']
 
-    # MONGO_URI = "mongodb://"
-    MONGO_URI = "mongodb://" + MONGO_USER + ":" + MONGO_PASS + "@" + MONGO_HOST + ":" + MONGO_PORT + "/" + MONGO_DB
+    MONGO_URI = os.environ['DB_URI']
+    # MONGO_URI = "mongodb://" + MONGO_USER + ":" + MONGO_PASS + "@" + MONGO_HOST + ":" + MONGO_PORT + "/" + MONGO_DB
+    # DB_URI = os.environ['DB_URI']
+    # MONGO
     # print(MONGO_URI)
+    # MONGO_NEW_URI =
+
 
     # MONGO_URI = os.environ['mlab_DB_URI']
     # MONGO_URI = os.environ['DB_URI']
