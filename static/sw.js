f223b08316cd356c076536091609025151d2c1b5
Hristo <hristo.meshinski@gmail.com>
2020-09-10 19:50:54 +0100

    fix vh100 issue on safari

--- static/sw.js
+++ static/sw.js
@@ -1,6 +1,6 @@
 // This is based on the First Progressive Web App Tutorial by Google
 // https://codelabs.developers.google.com/codelabs/your-first-pwapp/
-const cacheName = 'modern-spike-PWA-24';
+const cacheName = 'modern-spike-PWA-244';
 const filesToCache = [
     '/static/bootstrap/js/jquery-3.5.1.min.js',
     '/static/bootstrap/js/bootstrap.min.js',
