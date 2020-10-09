e8ca50fc7b718f4d6634b5ec326f6c28580bfc61
Hristo <hristo.meshinski@gmail.com>
2020-10-09 22:08:24 +0100

    update sw

--- static/sw.js
+++ static/sw.js
@@ -1,6 +1,6 @@
 // This is based on the First Progressive Web App Tutorial by Google
 // https://codelabs.developers.google.com/codelabs/your-first-pwapp/
-const cacheName = 'modern-spike-PWA-332';
+const cacheName = 'modern-spike-PWA-33322';
 const filesToCache = [
     '/static/bootstrap/js/jquery-3.5.1.min.js',
     '/static/bootstrap/js/bootstrap.min.js',
