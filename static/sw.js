a7ba2c32643cf1cda5f787a6aa4fb8404e839735
Hristo <hristo.meshinski@gmail.com>
2020-10-12 20:45:57 +0100

    changed illustration to artwork

--- static/sw.js
+++ static/sw.js
@@ -1,6 +1,6 @@
 // This is based on the First Progressive Web App Tutorial by Google
 // https://codelabs.developers.google.com/codelabs/your-first-pwapp/
-const cacheName = 'modern-spike-PWA-1.2.11';
+const cacheName = 'modern-spike-PWA-1.2.12';
 const filesToCache = [
     '/static/bootstrap/js/jquery-3.5.1.min.js',
     '/static/bootstrap/js/bootstrap.min.js',
