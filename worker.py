ebf5a4a8a15423ed2dc992f2a59b19773f13f458
hristo.meshinski@gmail.com <hristo.meshinski@gmail.com>
2022-09-16 00:34:31 +0300

    remove hard coded redis url

--- worker.py
+++ worker.py
@@ -5,7 +5,7 @@ from rq import Worker, Queue, Connection
 
 listen = ['high', 'default', 'low']
 
-redis_url = os.getenv('REDIS_URL', 'redis://:p655231bba6ef045cb05d5f5f543cc41e4de3a00d309941fbc2c5264d6b8e62e4@ec2-63-34-160-32.eu-west-1.compute.amazonaws.com:13309')
+redis_url = os.getenv('REDIS_URL', 'redis://localhost:6379')
 
 conn = redis.from_url(redis_url)
 
